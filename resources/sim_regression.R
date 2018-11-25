e1bfa733d3a61f2200cb3cc56c8e383efd0c74ec
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2018-11-25 16:05:10 -0500

    adding slides, update r packages i

--- resources/sim_regression.R
+++ resources/sim_regression.R
@@ -1,16 +1,14 @@
-sim_regression = function(n_samp, beta0, beta1) {
+sim_regression = function(n, beta0 = 2, beta1 = 3) {
   
   sim_data = tibble(
-    x = rnorm(n_samp, mean = 1, sd = 1),
-    y = beta0 + beta1 * x + rnorm(n_samp, 0, 1)
+    x = rnorm(n, mean = 1, sd = 1),
+    y = beta0 + beta1 * x + rnorm(n, 0, 1)
   )
   
-  ls_fit = lm(y ~ x, data = sim_data) %>% 
-    tidy()
+  ls_fit = lm(y ~ x, data = sim_data)
   
   tibble(
-    n = n_samp,
-    beta0_hat = pull(ls_fit, estimate)[1],
-    beta1_hat = pull(ls_fit, estimate)[2]
+    beta0_hat = coef(ls_fit)[1],
+    beta1_hat = coef(ls_fit)[2]
   )
 }
--- resources/simulate_nrep.R
+++
@@ -1,6 +0,0 @@
-simulate_nrep = function(n_rep, n_samp, beta0, beta1) {
-  
-  rerun(n_rep, sim_regression(n_samp, beta0, beta1)) %>% 
-    bind_rows()
-  
-}
